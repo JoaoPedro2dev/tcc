ede3817d544666c8fcc3d9c1d6a1cb8c9986b7d2
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-08-04 20:47:31 -0300

    conexão
    Adicionando arquivo de exemplo para conexão com o bd.

---
+++ API/CONFIG/conexao.example.php
@@ -0,0 +1,10 @@
+<?php 
+    //Preencha aas informações para a conexão
+
+    $_ENV['db']['host'] = 'nome do host';
+    $_ENV['db']['user'] = 'usuario';
+    $_ENV['db']['pass'] = 'senha';
+    $_ENV['db']['database'] = 'nome do banco de dados'; 
+
+    $_ENV['key'] = "chave de acesso para API";
+?>
